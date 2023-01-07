1e423e66d61b38020924fa1d196e693d0bfa4fee
ilhom <ilhommamatqulov276@gmail.com>
2023-01-07 18:02:24 +0500

    get_post_month.py

--- get_post_month.py
+++ get_post_month.py
@@ -13,4 +13,15 @@ def get_post_month(data:dict,month:int)->int:
         int: the number of posts for the given month
     """
     
-    return
+    for msg in messages:
+
+        if msg['type']=='message':
+            date=msg['date']
+            # Get month from date
+
+            if int(date[5:7])==month:
+                count+=1
+
+
+    return count
+
