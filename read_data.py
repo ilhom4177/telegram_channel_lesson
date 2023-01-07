f402ab38a16aff2aef568eea597d29fe241f7f10
ilhom <ilhommamatqulov276@gmail.com>
2023-01-07 17:59:53 +0500

    read_data.py

--- read_data.py
+++ read_data.py
@@ -11,4 +11,7 @@ def fromJson(file_path: str)->dict:
         dict: Dictionary containing the data of the json file.
     
     """
-    return
+    f = open(file_path, "r",encoding='utf8').read()
+    #load json
+    data = json.loads(f)
+    return data
