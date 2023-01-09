f57e76fcf072cef14478246884460740d2837b73
ilhom <ilhommamatqulov276@gmail.com>
2023-01-09 09:13:56 +0500

    get_post_month.py

--- get_post_per_month.py
+++ get_post_per_month.py
@@ -11,4 +11,22 @@ def get_post_per_month(data:dict)->dict:
     Returns: 
         dict: a dictionary with the number of posts for each month
     """
-    return
+    count = {
+        12:0,
+        7:0,
+        8:0,
+        9:0,
+        10:0,
+        11:0,
+        6:0,
+    }
+    messages = data['messages']
+    for msg in messages:
+        if msg['type']=='message':
+            date=msg['date']
+            count[int(date[5:7])]+=1
+    return count
+file_path = "data/result.json"
+data = fromJson(file_path)
+count = get_post_per_month(data)
+print(count)
